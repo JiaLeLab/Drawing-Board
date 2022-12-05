e638ce4e0b4f29aaa7269bc6bcd29fd668e1b9aa
Jia Le <88878287+JiaLe0709@users.noreply.github.com>
2022-12-05 22:58:00 +0800

    main

---
+++ canvas.js
@@ -0,0 +1,40 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+var radius = 10;
+var dragging = false;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function clearCanvas(canvas){
+    canvas.width = canvas.width;
+}
+
+context.lineWidth = radius * 2;
+
+var putPoint = function(e){
+    if(dragging){
+    context.lineTo(e.clientX, e.clientY);
+    context.stroke();
+    context.beginPath();
+    context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
+    context.fill();
+    context.beginPath();
+    context.moveTo(e.clientX, e.clientY);
+    }
+}
+
+var engage = function(e){
+    dragging = true;
+    putPoint(e);
+}
+
+var disengage = function(){
+    dragging = false;
+    context.beginPath();
+}
+
+canvas.addEventListener('mousedown', engage);
+canvas.addEventListener('mousemove', putPoint);
+canvas.addEventListener('mouseup', disengage);
